c6a00b949955db53fad570ccb093bd9721d2f031
ByronLeal <dev7CE@hotmail.com>
2021-09-24 01:21:47 -0600

    Refactor: web default  route removed

--- todays-journal-api/routes/web.php
+++ todays-journal-api/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Only API routes shoul be enabled
+// Route::get('/', function () {
+//     return view('welcome');
+// });
