766e0e63b424bed047419a98d8c42b7459d98738
ByronLeal <dev7CE@hotmail.com>
2021-09-24 01:28:29 -0600

    Refact:  can_fecth_a_single_model function renamed

--- todays-journal-api/tests/Feature/Stories/ListStoriesTest.php
+++ todays-journal-api/tests/Feature/Stories/ListStoriesTest.php
@@ -12,7 +12,7 @@ class ListStoriesTest extends TestCase
     use RefreshDatabase;
     
     /** @test */
-    public function can_fetch_a_single_article()
+    public function can_fetch_a_single_story()
     {
         // Show specific errors
         $this->withoutExceptionHandling();
@@ -43,5 +43,48 @@ class ListStoriesTest extends TestCase
             ]
         ]);
 
+    }
+    
+    /** @test */
+    public function can_fetch_all_stories()
+    {
+        // Show specific errors
+        $this->withoutExceptionHandling();
+        
+        $stories = factory(Story::class, 1)->create();
+
+        $response = $this->getJson(route('api.v1.stories.index'));
+
+        $response->assertExactJson([
+            'data' => [
+                [
+                    'type' => 'stories',
+                    'id' => (string) $stories[0]->getRouteKey(),
+                    'attributes' => [
+                        'title' => $stories[0]->title,
+                        'url' => $stories[0]->url,
+                        'content' => $stories[0]->content
+                    ],
+                    'links' => [
+                        'self' => route('api.v1.stories.show', $stories[0]) 
+                    ]
+                ],
+                // [
+                //     'type' => 'stories',
+                //     'id' => (string) $stories[1]->getRouteKey(),
+                //     'attributes' => [
+                //         'title' => $stories[1]->title,
+                //         'url' => $stories[1]->url,
+                //         'content' => $stories[1]->content
+                //     ],
+                //     'links' => [
+                //         'self' => route('api.v1.stories.show', $stories[1]) 
+                //     ]
+                // ]
+            ],
+            'links' => [
+                'self' => route('api.v1.stories.index'),
+            ]
+        ]);
     }
 }
